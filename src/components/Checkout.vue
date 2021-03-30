c55882580b0c461a1646cca04da787bb561f3eba
jdmorales <john@rocka.co>
2021-03-29 23:43:45 -0500

    Third step: refactoring code

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="checkout-component">
-    <form>
-      <label for="coupon-input">Enter a coupon</label>
-      <input
-        id="coupon-input"
-        type="text"
-      >
-    </form>
+    <div class="wrapper-form">
+      <form>
+        <div class="form-group">
+          <label for="coupon-input">Enter a coupon</label>
+          <input
+            id="coupon-input"
+            v-model="coupon"
+            type="text"
+          >
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 export default{
   name: 'Checkout',
+  data(){
+    return {
+      coupon: ''
+    }
+  },
   methods: {}
 }
 </script>
+
+<style lang="css" scoped>
+.checkout-component{
+  display: flex;
+  justify-content: center;
+}
+
+.wrapper-form{
+  background-color: #f0efef;
+  padding: 32px;
+  border-radius: 8px;
+}
+
+.form-group{
+  display: flex;
+  flex-direction: column;
+}
+</style>
