67c0ab312350de27ff3b25bda756ddfe1d7a9c9c
jdmorales <john@rocka.co>
2021-03-29 23:56:17 -0500

    Fourth step: make the tests fail.

--- tests/unit/Checkout.spec.js
+++ tests/unit/Checkout.spec.js
@@ -5,7 +5,7 @@ import Checkout from '@/components/Checkout'
 
 describe('test on checkout', () => {
   it('should exist inputs necessary in checkout', async() => {
-    expect.assertions(1)
+    expect.assertions(4)
     // arrange
     const { debug } = render(Checkout)
     // eslint-disable-next-line testing-library/no-debug
@@ -14,8 +14,17 @@ describe('test on checkout', () => {
     /*
     * Search types: https://testing-library.com/docs/vue-testing-library/cheatsheet#search-types
     */
-    // expect if exist exist the input about coupon
+    // assert: if exist exist the input about coupon
     const couponInput = screen.getByLabelText('Enter a coupon')
     expect(couponInput).toBeTruthy()
+
+    // assert: if exist the submit button and total to pay
+    expect(screen.getByRole('button')).toBeTruthy()
+
+    //assert: if exist total to pay
+    expect(screen.getByTestId('total-before-pay')).toBeTruthy()
+
+    //assert: if exist total to pay
+    expect(screen.getByTestId('total-to-pay')).toBeTruthy()
   })
 })
